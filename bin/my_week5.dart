9bc915b2d896b0beb8fcdf1809705d680be7e5f2
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-06-16 17:29:18 +0700

    Update

--- bin/my_week5.dart
+++ bin/my_week5.dart
@@ -1,11 +1,10 @@
-void main(){
-  //Object cat1
-  Cat cat1 = Cat("American",10,101);
-  Cat cat2 = Cat.catMeow(name:"Balinese",age:12,102,"red");
-  //print('${cat1.catName} ${cat1.catAge} ${cat1.catId}');
+void main() {
+  Cat cat1 = Cat("American", 10, 101);//Object cat1
+  // Cat cat2 = Cat.catMeaw("Balinese ", 12, 102,"red");
+  Cat cat2 = Cat.catMeaw(name: "Balinese ", age: 12, id: 102, color: "red");
 }
 
-class Cat{
+class Cat {
   //Attribute
   String? catName;
   int? catAge;
@@ -13,18 +12,18 @@ class Cat{
   String? catColor;
 
   //Create Constructor
-  Cat(String name,int age,int id){
+  Cat(String name, int age, int id) {
     catName = name;
     catAge = age;
     catId = id;
-    print('Name:$catName, Age:$catAge ID:$catId');
+    print('Name:$catName, Age:$catAge, ID:$catAge');
   }
-  //Named constructor Cat.catMeow(String name,int age,int id,String color){
-  Cat.catMeow({String? name, int? age, int? id,String color}){
+  //Named Constructor: Cat.catMeaw(String name, int age, int id, String color){
+  Cat.catMeaw({String? name, int? age, int? id, String? color}) { //Named parameter
     catName = name;
     catAge = age;
     catId = id;
     catColor = color;
-    print('Name:$catName, Age:$catAge ID:$catId,Color:$catColor');
+    print('Name:$catName, Age:$catAge, ID:$catAge,Color:$catColor');
   }
 }
